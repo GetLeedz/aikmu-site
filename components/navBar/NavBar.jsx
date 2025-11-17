8b1f899586c26384cd7b0b3ef938d881ba21efc6
GetLeedz <81183998+GetLeedz@users.noreply.github.com>
2025-11-17 09:44:26 +0100

    Update NavBar.jsx

--- components/navBar/NavBar.jsx
+++ components/navBar/NavBar.jsx
@@ -3,28 +3,31 @@ import Link from "next/link";
 import { useState } from "react";
 import { FaBars } from "react-icons/fa";
 import Logo from "../../public/images/getleedz-logo.png";
-//import NavItems from "../navItems/NavItems";
+// import NavItems from "../navItems/NavItems";
 import Social from "../social/Social";
 
 const NavBar = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    // Header-Balken: weniger hoch, direkt am Top
-    <nav className="fixed top-0 w-full z-50 backdrop-blur-xl">
-      {/* navbar brand + Inhalt */}
-      <div className="px-4 xl:mx-[55px] 2xl:mx-[120px] flex flex-row tablet:flex-col items-center tablet:items-start gap-7 xl:gap-[45px] 2xl:gap-[84px] py-4 relative">
+    <nav className="fixed top-0 w-full flex flex-col z-50 py-[30px] md:py-[40px] backdrop-blur-xl">
+      {/* navbar brand */}
+      <div className="px-4 xl:mx-[55px] 2xl:mx-[120px] flex flex-row tablet:flex-col items-start tablet:items-start gap-7 xl:gap-[45px] 2xl:gap-[84px] relative">
         
-        {/* Logo-Box etwas höher, damit dein Logo sauber sitzt */}
-        <div className="w-[146px] h-[90px] flex items-center">
+        {/* Logo – auf Mobile kleiner */}
+        <div className="relative w-[160px] h-[40px] sm:w-[190px] sm:h-[48px] lg:w-[220px] lg:h-[55px]">
           <Link href="/">
-            <a className="block w-full h-full relative">
-              <Image src={Logo} alt="Nav Bar Brand" layout="fill" objectFit="contain" />
-            </a>
+            <Image
+              src={Logo}
+              alt="GetLeedz Logo"
+              layout="fill"
+              objectFit="contain"
+              priority
+            />
           </Link>
         </div>
 
-        {/* Burger-Icon für Tablet/Mobile */}
+        {/* Burger Menü */}
         <div
           tabIndex={0}
           onClick={() => setOpen(!open)}
@@ -33,14 +36,17 @@ const NavBar = () => {
           <FaBars className="text-xl" />
         </div>
 
-        {/* rechter Bereich: Social + Button */}
+        {/* rechte Seite */}
         <div
           className={`flex flex-row tablet:flex-col items-center tablet:items-start justify-between w-full ${
             open ? "tablet:block" : "tablet:hidden"
           }`}
         >
-
-
+          {/* Social icons + Button */}
+          <div className="flex flex-row tablet:flex-col items-center tablet:items-start gap-5 xl:gap-8 2xl:gap-12">
+            <Social />
+            {/* CTA rechts oben optional – falls du den nicht willst, einfach weg lassen */}
+          </div>
         </div>
       </div>
     </nav>
