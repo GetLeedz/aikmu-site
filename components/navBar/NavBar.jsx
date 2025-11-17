94be3686b1c3c5e53b354e9cb3efc0f71c99ee58
GetLeedz <81183998+GetLeedz@users.noreply.github.com>
2025-11-17 09:54:47 +0100

    Update NavBar.jsx

--- components/navBar/NavBar.jsx
+++ components/navBar/NavBar.jsx
@@ -3,7 +3,8 @@ import Link from "next/link";
 import { useState } from "react";
 import { FaBars } from "react-icons/fa";
 import Logo from "../../public/images/getleedz-logo.png";
-
+// import NavItems from "../navItems/NavItems";
+import Social from "../social/Social";
 
 const NavBar = () => {
   const [open, setOpen] = useState(false);
