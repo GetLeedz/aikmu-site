d4b9e93045f2fd8fa6e62d97c8cf280aeacbe139
GetLeedz <81183998+GetLeedz@users.noreply.github.com>
2025-11-17 15:54:30 +0100

    Update NavBar.jsx

--- components/navBar/NavBar.jsx
+++ components/navBar/NavBar.jsx
@@ -15,10 +15,9 @@ const NavBar = () => {
         {/* LOGO – linksbündig */}
         <Link href="/" legacyBehavior>
           <a className="flex items-center">
-{/* ✅ NEU (mit negativen Margins, um das Padding des Elternelements auszugleichen) */}
-<div className="relative w-[170px] h-[46px] sm:w-[210px] sm:h-[56px] lg:w-[240px] lg:h-[64px] -ml-3 sm:-ml-5 lg:-ml-10">
-  {/* Inhalt Button o.ä. */}
-</div>
+              {/* ✅ NEU (mit negativen Margins, um das Padding des Elternelements auszugleichen) */}
+              <div className="relative w-[170px] h-[46px] sm:w-[210px] sm:h-[56px] lg:w-[240px] lg:h-[64px] -ml-3 sm:-ml-5 lg:-ml-10">
+
 
               <Image
                 src={Logo}
