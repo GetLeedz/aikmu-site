3850007afce675b9426fece7611b60218325fd19
GetLeedz <81183998+GetLeedz@users.noreply.github.com>
2025-12-14 15:07:59 +0100

    Update NavBar.jsx

--- components/navBar/NavBar.jsx
+++ components/navBar/NavBar.jsx
@@ -5,14 +5,17 @@ import Logo from "../../public/images/getleedz-logo.png";
 
 const NavBar = () => {
   return (
-    // Fix oben, voller Balken mit Blur
-    <nav className="fixed inset-x-0 top-0 z-50">
-      {/* Top-Banner (blur + dunkler Hintergrund) */}
-      <div className="backdrop-blur-xl bg-[#020617]/70 border-b border-white/5">
-        {/* Logo zentriert, etwas Abstand nach unten */}
+    // Fixierter Header – schneidet alles darunter sauber ab
+    <nav className="fixed inset-x-0 top-0 z-50 overflow-hidden">
+      {/* Top-Banner mit Blur & dunklem Hintergrund */}
+      <div className="backdrop-blur-xl bg-[#020617]/80 border-b border-white/5">
+        {/* Logo zentriert */}
         <div className="flex justify-center py-3">
           <Link href="/" legacyBehavior>
-            <a className="block relative w-[200px] h-[60px] sm:w-[240px] sm:h-[72px] lg:w-[260px] lg:h-[80px]">
+            <a
+              aria-label="GetLeedz Home"
+              className="relative block w-[200px] h-[60px] sm:w-[240px] sm:h-[72px] lg:w-[260px] lg:h-[80px]"
+            >
               <Image
                 src={Logo}
                 alt="GetLeedz Logo"
